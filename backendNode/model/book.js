d87956deae61c913769a01967d2f5a80b029231e
misaelh2015 <61474910+misaelh2015@users.noreply.github.com>
2023-04-17 23:13:59 -0500

    made the schema for the book store

--- backendNode/model/book.js
+++ backendNode/model/book.js
@@ -1,1 +1,21 @@
 //schema will be drafted up for the project app
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Book = new Schema({
+    name: {
+        type: String
+    },
+    price: {
+        type: String
+    },
+    description: {
+        type: String
+    }
+},
+    {
+        collection: 'books'
+
+    })
+//you have to export the model
+module.exports = mongoose.model('Book', Book)
