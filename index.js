ceea705422286a3a3faf41cdc1987faa2a53b573
misaelh2015 <61474910+misaelh2015@users.noreply.github.com>
2023-04-21 02:36:58 -0500

    index file setup for db connection

---
+++ index.js
@@ -0,0 +1,17 @@
+// im going to write out the connection code for our db
+
+let express = require('express'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    mongoDB = require('./database/db');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoDB.bookstore, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(() => {
+    console.log("Database connected successfully")
+})
